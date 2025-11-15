7a9ad9cf7fc32a0181561a8b736147c1f0253e32
arcde40 <arcde40@gmail.com>
2025-11-15 15:51:26 +0900

    chore: openapi profile

--- src/main/kotlin/com/inthon/kourse/system/config/OpenApiConfig.kt
+++ src/main/kotlin/com/inthon/kourse/system/config/OpenApiConfig.kt
@@ -6,11 +6,15 @@ import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
 
 @Configuration
 class OpenApiConfig {
 
     @Bean
+    @Profile("dev")
+    @Primary
     fun customOpenAPI(): OpenAPI {
         return OpenAPI()
             .info(
@@ -28,6 +32,29 @@ class OpenApiConfig {
                     Server()
                         .url("http://localhost:8080")
                         .description("Local Development Server")
+                )
+            )
+    }
+
+    @Bean
+    @Profile("deploy")
+    fun customOpenAPIDeploy(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Kourse API")
+                    .version("1.0.0")
+                    .description("2025 iNThon 3th Team - Course Management API")
+                    .contact(
+                        Contact()
+                            .name("iNThon Team")
+                    )
+            )
+            .servers(
+                listOf(
+                    Server()
+                        .url("http://inthon.fjey.me:8080")
+                        .description("Remote Deployed Server")
                 )
             )
     }
