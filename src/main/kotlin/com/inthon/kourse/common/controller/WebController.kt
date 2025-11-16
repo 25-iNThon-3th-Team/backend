09fb43d8f3bee03a1e87b9c758c9726af7e83055
arcde40 <arcde40@gmail.com>
2025-11-16 12:03:53 +0900

    chore: webcontroller

---
+++ src/main/kotlin/com/inthon/kourse/common/controller/WebController.kt
@@ -0,0 +1,14 @@
+package com.inthon.kourse.common.controller
+
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+
+@Controller
+class WebController : ErrorController {
+    @GetMapping("/", "/error")
+    fun index(): String {
+        return "index.html"
+    }
+}
